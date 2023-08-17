2e9f9065d503e808f6142bd5e85e5944895cf4fe
awangui <86366610+awangui@users.noreply.github.com>
2023-08-17 08:49:45 +0300

    portfolio starter files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
 function hideLoader(){
   $('.page-loader').fadeOut('slow');
 }
+function reveal() {
+  var reveals = document.querySelectorAll(".fade-in");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+function slide() {
+  var reveals = document.querySelectorAll(".slide-in");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 200;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+function rightSlide() {
+  var reveals = document.querySelectorAll(".right-slide-in");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 200;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", function() { reveal(); slide(); rightSlide();});
+
+
+
+// To check the scroll position on page load
+reveal();
+slide();
+rightSlide();
